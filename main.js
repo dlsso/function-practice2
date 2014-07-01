ad6534060f5c1f67cb7da89c073682bbb9fed174
dlsso <supertonesfan@hotmail.com>
2014-06-30 19:17:05 -0600

    Solved first 3

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-$(document).on('ready', function() {
+function getName(object) {
+	return object.name;
+}
+console.log(getName({ name: 'Luisa', age: 25 }))
 
 
+function totalLetters(array) {
+	return array.join('').length
+}
+console.log(totalLetters(['what', 'happened', 'to', 'my', 'function']))
 
-	
-});
+// function keyValue(key, value) {
+// 	this.key = value
+// }
+
+// function Shape(x, y) {
+//     this.x= x;
+//     this.y= y;
+// }
+
+function keyValue(key, value) {
+	var object = {}
+	object[key] = value
+	return object
+
+}
